399e4bce81d5f38ae4625707dbbb55bbe2b66198
随谕 <bluesking2019@foxmail.com>
2020-07-26 00:06:37 +0800

    chore(*): 初始化项目结构 (#1)
    * chore(*): 新增 Travis CI 配置文件
    
    * feat(*): 搭建出 Maven 多项目结构
    
    Co-authored-by: 随心 <1023293003@qq.com>

---
+++ bank-start/src/main/java/cn/bluesking/bank/BankApplication.java
@@ -0,0 +1,19 @@
+package cn.bluesking.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Bank 项目启动类。
+ *
+ * @author 随心
+ * @date 2020/7/25
+ */
+@SpringBootApplication
+public class BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApplication.class, args);
+    }
+
+}
